d86792726f9f99b6b1f89f4ef997b7c69a6f6635
BatuSengezer <bsengezer@ku.edu.tr>
2023-12-26 23:01:06 +0100

    filename fix

--- ex05/srcs/Harl.cpp
+++ ex05/srcs/Harl.cpp
@@ -6,17 +6,47 @@
 /*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 16:20:06 by bsengeze          #+#    #+#             */
-/*   Updated: 2023/12/26 16:20:07 by bsengeze         ###   ########.fr       */
+/*   Updated: 2023/12/26 22:47:10 by bsengeze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main() {
-  Harl harl;
-  harl.complain("DEBUG");
-  harl.complain("INFO");
-  harl.complain("WARNING");
-  harl.complain("ERROR");
-  return 0;
+void Harl::debug() {
+  std::cout
+      << "I love having extra bacon for my "
+         "7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+      << std::endl;
+}
+
+void Harl::info() {
+  std::cout
+      << "I cannot believe adding extra bacon costs more money. You didn’t put "
+         "enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+      << std::endl;
+}
+
+void Harl::warning() {
+  std::cout
+      << "I think I deserve to have some extra bacon for free. I’ve been "
+         "coming for years whereas you started working here since last month."
+      << std::endl;
+}
+
+void Harl::error() {
+  std::cout << "This is unacceptable! I want to speak to the manager now."
+            << std::endl;
+}
+
+void Harl::complain(std::string level) {
+  void (Harl::*complaints[])(void) = {&Harl::debug, &Harl::info, &Harl::warning,
+                                      &Harl::error};
+  std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+  for (int i = 0; i < 4; ++i) {
+    if (level == levels[i]) {
+      (this->*complaints[i])();
+      break;
+    }
+  }
 }
--- ex05/srcs/main.cpp
+++ ex05/srcs/main.cpp
@@ -6,47 +6,17 @@
 /*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 16:20:10 by bsengeze          #+#    #+#             */
-/*   Updated: 2023/12/26 16:20:11 by bsengeze         ###   ########.fr       */
+/*   Updated: 2023/12/26 22:50:39 by bsengeze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-void Harl::debug() {
-  std::cout
-      << "I love having extra bacon for my "
-         "7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
-      << std::endl;
-}
-
-void Harl::info() {
-  std::cout
-      << "I cannot believe adding extra bacon costs more money. You didn’t put "
-         "enough bacon in my burger! If you did, I wouldn’t be asking for more!"
-      << std::endl;
-}
-
-void Harl::warning() {
-  std::cout
-      << "I think I deserve to have some extra bacon for free. I’ve been "
-         "coming for years whereas you started working here since last month."
-      << std::endl;
-}
-
-void Harl::error() {
-  std::cout << "This is unacceptable! I want to speak to the manager now."
-            << std::endl;
-}
-
-void Harl::complain(std::string level) {
-  void (Harl::*complaints[])(void) = {&Harl::debug, &Harl::info, &Harl::warning,
-                                      &Harl::error};
-  std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-
-  for (int i = 0; i < 4; ++i) {
-    if (level == levels[i]) {
-      (this->*complaints[i])();
-      break;
-    }
-  }
+int main() {
+  Harl harl;
+  harl.complain("DEBUG");
+  harl.complain("INFO");
+  harl.complain("WARNING");
+  harl.complain("ERROR");
+  return 0;
 }
